a65f2ffbc59a42bd6e9ae581569cba5f9e717af2
KlaitonRicken <klaitonricken@hotmail.com>
2020-04-12 15:32:34 -0300

    Adição de verificação de jogadas feitas

--- src/Main.java
+++ src/Main.java
@@ -24,26 +24,35 @@ public class Main {
 		tbi[1][2] = "8";
 		tbi[2][1] = "6";
 
-		/*tbf[0][0] = "2";
-		tbf[1][0] = "4";
-		tbf[2][0] = "3";
-		tbf[0][1] = "1";
-		tbf[0][2] = "5";
-		tbf[1][1] = " ";
-		tbf[2][2] = "7";
-		tbf[1][2] = "8";
-		tbf[2][1] = "6";*/
+//		tbf[0][0] = "1";
+//		tbf[1][0] = " ";
+//		tbf[2][0] = "2";
+//		tbf[0][1] = "4";
+//		tbf[0][2] = "5";
+//		tbf[1][1] = "8";
+//		tbf[2][2] = "6";
+//		tbf[1][2] = "7";
+//		tbf[2][1] = "3";
 		
-		tbf[0][0] = "1";
-		tbf[1][0] = "2";
-		tbf[2][0] = " ";
+		/*tbf[0][0] = "1";
+		tbf[1][0] = " ";
+		tbf[2][0] = "2";
 		tbf[0][1] = "4";
 		tbf[0][2] = "5";
 		tbf[1][1] = "8";
 		tbf[2][2] = "6";
 		tbf[1][2] = "7";
-		tbf[2][1] = "3";
+		tbf[2][1] = "3";*/
 		
+		tbf[0][0] = "2";
+		tbf[1][0] = "3";
+		tbf[2][0] = "1";
+		tbf[0][1] = "7";
+		tbf[0][2] = "5";
+		tbf[1][1] = "4";
+		tbf[2][2] = "6";
+		tbf[1][2] = "8";
+		tbf[2][1] = " ";
 		
 		tbInicial.tabuleiro = tbi;
 		tbFinal.tabuleiro = tbf;
@@ -60,25 +69,103 @@ public class Main {
 				
 				case 1: //Busca em profundidade interativa
 					
-					int limiteDeProfundidade;
-					boolean verificaLimite = true;
+					int limiteDeProfundidade; //limite de profundidade da busca
+					boolean verificaLimite = true; //verificação para o limite da busca
+					
+					Fronteira fronteiraFinal = new Fronteira(); //guardar o resultado final da busca
+					
+					fronteiraFinal = verificaFronteira(FronteiraAtual, tbFinal);
 					
 					limiteDeProfundidade = Integer.parseInt(JOptionPane.showInputDialog("Insira o limite de profundidade"));
 					
-					while(verificaFronteira(FronteiraAtual, tbFinal) == false && verificaLimite == true) { //|| profundidade != x
+					do{ // Enquanto não retornar o resultado correto ou o limite de profundidade não ser atingido
 						
-						//JOptionPane.showMessageDialog(null, FronteiraAtual.get(0).profundidade);
-						
-						if(FronteiraAtual.get(0).profundidade<limiteDeProfundidade && FronteiraAtual.get(0).expansao == false)
-							expandeFronteira(FronteiraAtual.get(0),FronteiraAtual);
+						//Se o primeiro da lista não tiver em sua profundidade máxima e ainda não foi expandido
+						if(FronteiraAtual.get(0).profundidade < limiteDeProfundidade && FronteiraAtual.get(0).expansao == false)
+							expandeFronteira(FronteiraAtual.get(0),FronteiraAtual); //expande a fronteira
 						else
-							FronteiraAtual.remove(0);
+							FronteiraAtual.remove(0); //Se não, retira da lista, pois já foi expandido, ou seu limite de profundidade já foi atingido
 						
-						verificaLimite = limiteDeProfundidade(FronteiraAtual, limiteDeProfundidade);
-					}
+						//Verifica se todos os nós foram expandidos ao seu limite
+						verificaLimite = limiteDeProfundidade(FronteiraAtual, fronteiraFinal, limiteDeProfundidade);
+						
+						//Verifica se já achou o resultado dentro das fronteiras atuais
+						fronteiraFinal = verificaFronteira(FronteiraAtual, tbFinal);
+						
+					}while(fronteiraFinal == null && verificaLimite == true);  // Enquanto não retornar o resultado correto ou o limite de profundidade não ser atingido
 
-					if( verificaLimite == false) {
+					if( verificaLimite == false) { //Se o limite for atingido e ainda não encontrou o resultado
 						JOptionPane.showMessageDialog(null, "Resultado não encontrado até a profundidade " + limiteDeProfundidade);
+					}
+					
+					//MOSTRA RESULTADOS FINAIS
+					if(fronteiraFinal != null) {
+						
+						String tabuleiroFinal = "-------------\n";
+						
+						for(int i = 0; i<=2; i++) {
+							tabuleiroFinal += "| ";
+							for(int j = 0; j<=2; j++) {
+								tabuleiroFinal += tbFinal.tabuleiro[i][j] + " | ";
+							}
+							
+							tabuleiroFinal += "\n-------------\n";
+						}
+						
+						
+						String tabuleiro = "-------------\n";
+						
+						for(int i = 0; i<=2; i++) {
+							tabuleiro += "| ";
+							for(int j = 0; j<=2; j++) {
+								tabuleiro += fronteiraFinal.tabuleiro[i][j] + " | ";
+							}
+							
+							tabuleiro += "\n-------------\n";
+						}
+						
+						JOptionPane.showMessageDialog(null,"Tabuleiro Final : \n" + tabuleiroFinal + "\n Resultado Final : \n" + tabuleiro
+								+ "\n Foi encontrado na profundidade: " + fronteiraFinal.profundidade);
+						
+						// MOSTRA MOVIMENTOS NO CONSOLE
+				
+						String caminho = "";
+						int n = fronteiraFinal.profundidade + 1;
+						
+						caminho += "-------------\n";
+						
+						for(int i = 0; i<=2; i++) {
+							caminho += "| ";
+							for(int j = 0; j<=2; j++) {
+								caminho += fronteiraFinal.tabuleiro[i][j] + " | ";
+							}
+							
+							caminho += "\n-------------\n";
+						}
+						
+						caminho += " Nº de movimentos: "+ n-- + "\n \n";
+						
+						do {
+							
+							fronteiraFinal = fronteiraFinal.antecessor;
+	
+							caminho += "-------------\n";
+							
+							for(int i = 0; i<=2; i++) {
+								caminho += "| ";
+								for(int j = 0; j<=2; j++) {
+									caminho += fronteiraFinal.tabuleiro[i][j] + " | ";
+								}
+								caminho += "\n-------------\n";
+							}
+							
+							caminho += " Nº de movimentos: "+ n-- + "\n \n";
+							
+						}while(!fronteiraFinal.toString().equals(tbInicial.toString()));
+						
+						System.out.println(caminho);
+						
+						JOptionPane.showMessageDialog(null, "Os movimentos feitos para a resolução do jogo podem ser encontrados no console");
 					}
 						
 					break;
@@ -112,58 +199,36 @@ public class Main {
 	}
 	
 	
-	public static boolean verificaFronteira(ArrayList<Fronteira> fronteiraAtual, Fronteira fronteira ) {
+	public static Fronteira verificaFronteira(ArrayList<Fronteira> fronteiraAtual, Fronteira fronteira ) {
 		
 		boolean verifica = false;
 		Fronteira f = new Fronteira();
+		f.profundidade = 0;
 		
 		for(int i=0; i<fronteiraAtual.size();i++) {
 			if(fronteiraAtual.get(i).toString().equals(fronteira.toString())) {
-				verifica = true;
-				f = fronteiraAtual.get(i);
+				//if(fronteiraAtual.get(i).profundidade < f.profundidade) {
+					verifica = true;
+					f = fronteiraAtual.get(i);
+				//}
 			}
 		}
 		
-		if(verifica == true) {
-			
-			String tabuleiroFinal = "-------------\n";
-			
-			for(int i = 0; i<=2; i++) {
-				tabuleiroFinal += "| ";
-				for(int j = 0; j<=2; j++) {
-					tabuleiroFinal += fronteira.tabuleiro[i][j] + " | ";
-				}
-				
-				tabuleiroFinal += "\n-------------\n";
-			}
-			
-			
-			String tabuleiro = "-------------\n";
-			
-			for(int i = 0; i<=2; i++) {
-				tabuleiro += "| ";
-				for(int j = 0; j<=2; j++) {
-					tabuleiro += f.tabuleiro[i][j] + " | ";
-				}
-				
-				tabuleiro += "\n-------------\n";
-			}
-			
-			JOptionPane.showMessageDialog(null,"Tabuleiro Final : \n" + tabuleiroFinal + "\n Resultado Final : \n" + tabuleiro);
-		}
-		
-		return verifica;
+		if(verifica == true)
+			return f;
+		else
+			return null;
 		
 	}
 	
 	public static void expandeFronteira(Fronteira f, ArrayList<Fronteira> fronteiras) {
 		
-		if(f.expansao==false) {
+		if(f.expansao==false) { //Verifica se o no já foi expandido
 			
-			f.expansao = true;
+			f.expansao = true; //Caso não tenha sido expandido, ele será expandido, setando agora a variavel expansao como verdadeira
 			
-			int pI=0; 
-			int pJ=0;
+			int pI=0; //Para recuperar a posição da peça vazia
+			int pJ=0; //Para recuperar a posição da peça vazia
 			
 			for(int i=0;i<3;i++) {   // Laço para verificar a posição da peça vazia
 				for(int j=0;j<3;j++) {
@@ -177,9 +242,9 @@ public class Main {
 			
 			//JOptionPane.showMessageDialog(null, "Posição i: " + pI + "Posição j: " + pJ + " - ");
 			
-					/* tb.tabuleiro[i][j] tem a posição da peça atual, ex (i,j) = (1,1)
+					/* f.tabuleiro[i][j] tem a posição da peça atual, ex (i,j) = (1,1)
 					 * se subtrairmos o contador i (i--), ficamos com (i--,j) = (0,1)
-					 * Isso nos dará as peças que fazem fronteira com a peça atual, na vertical e horizontal
+					 * Isso nos dará a peça vizinha com a peça atual
 					 * 
 					 * Posição atual = PA (peça vazia na qual vai ser movida)
 					 * 
@@ -193,7 +258,7 @@ public class Main {
 					 * 	 ----------
 					 * 
 					 * Se diminuirmos i-- e o valor for maior que zero, significa que esta posição existe na matriz
-					 * e a peça na POSIÇÃO ANALISADA (XX) faz fronteira com PA
+					 * e a peça na POSIÇÃO ANALISADA (XX) é vizinha de PA
 					 * 
 					 *    0  1  2  
 					 * 	 ----------
@@ -204,8 +269,8 @@ public class Main {
 					 * 2 |  |  |  |
 					 * 	 ----------
 					 * 
-					 * Se somarmos i++ e o valor for menor que dois, significa quue esta posição existe na matriz
-					 * e a peça na POSIÇÃO ANALISADA (XX) faz fronteira com PA
+					 * Se somarmos i++ e o valor for menor que dois, significa que esta posição existe na matriz
+					 * e a peça na POSIÇÃO ANALISADA (XX) é vizinha de PA
 					 * 
 					 *    0  1  2  
 					 * 	 ----------
@@ -243,48 +308,29 @@ public class Main {
 					
 					novaFronteiraParaCima.tabuleiro = tbParaCima; 
 					
-					tbParaCima[pI][pJ] = tbParaCima[pI - 1][pJ]; 
+					tbParaCima[pI][pJ] = tbParaCima[pI - 1][pJ]; //Faz os movimentos do tabuleiro
 					tbParaCima[pI - 1][pJ] = " ";
 					
 					//JOptionPane.showMessageDialog(null, novaFronteiraParaCima.toString() +"\n"+ f.antecessor.toString());
 					
+					//Verifica se o tabuleiro criado não é igual ao antecessor de seu antecessor (se não está voltando a uma joga que já foi feita)
 					if(!novaFronteiraParaCima.toString().equals(f.antecessor.toString())) { //CASO SEJA IGUAL, ESSA FRONTEIRA JÁ EXISTE
 						
-						novaFronteiraParaCima.antecessor = f;
-						novaFronteiraParaCima.profundidade = f.profundidade + 1;
+						novaFronteiraParaCima.antecessor = f; //Guarda seu antecessor para recuperar o caminho
+						novaFronteiraParaCima.profundidade = f.profundidade + 1; //Guarda a profundidade
 						
 						fronteiras.add(0,novaFronteiraParaCima);  //Adiciona a nova fronteira na lista de fronteiras
-						
 						//mostraMovimento(novaFronteiraParaCima);
 					} 
 				}
-						/*    0  1  2  
-						 * 	 ----------
-						 * 0 |1 |7 |4 |   
-						 * 	 ----------     
-						 * 1 |5 |  |2 |     move a peça vazia para cima e a com valor para baixo 
-						 * 	 ----------
-						 * 2 |3 |8 |6 | 
-						 * 	 ---------
-						 *
-						 *    0  1  2  
-						 * 	 ----------
-						 * 0 |1 |  |4 |   
-						 * 	 ----------     Peça movida foi a 7
-						 * 1 |5 |7 |2 |     
-						 * 	 ----------
-						 * 2 |3 |8 |6 | 
-						 * 	 ----------
-						*/
-					//pI++;
 					
 				if((pJ - 1) >= 0) { //Verifica se existe uma posição à Esquerda
 					
-					Fronteira novaFronteiraEsquerda = new Fronteira(); //Cria nova fronteira
+					Fronteira novaFronteiraEsquerda = new Fronteira(); 
 					
 					String tbEsquerda [][] =  new String [3][3];
 					
-					for(int i=0;i<=2;i++) {   // Laço para verificar a posição da peça vazia
+					for(int i=0;i<=2;i++) {   
 						for(int j=0;j<=2;j++) {
 							tbEsquerda[i][j] = f.tabuleiro[i][j];
 							}
@@ -297,15 +343,13 @@ public class Main {
 					
 					//JOptionPane.showMessageDialog(null, novaFronteiraEsquerda.toString() +"\n"+ f.antecessor.toString());
 					
-					if(!novaFronteiraEsquerda.toString().equals(f.antecessor.toString())) { //cASO SEJA IGUAL, ESSA FRONTEIRA JÁ EXISTE
+					if(!novaFronteiraEsquerda.toString().equals(f.antecessor.toString())) { 
 				
 						novaFronteiraEsquerda.antecessor = f;
 						novaFronteiraEsquerda.profundidade = f.profundidade + 1;
 						
 						//f.fronteiras.add(novaFronteiraEsquerda);
-						
-						fronteiras.add(0,novaFronteiraEsquerda);  //Adiciona a nova fronteira na lista de fronteiras
-			
+						fronteiras.add(0,novaFronteiraEsquerda);
 						//mostraMovimento(novaFronteiraEsquerda);
 						
 					}
@@ -314,11 +358,11 @@ public class Main {
 					
 				if((pI + 1) <= 2) {  //Verifica se existe uma posição para baixo
 					
-					Fronteira novaFronteiraParaBaixo = new Fronteira(); //Cria nova fronteira
+					Fronteira novaFronteiraParaBaixo = new Fronteira();
 	
 					String tbParaBaixo [][] =  new String [3][3];
 					
-					for(int i=0;i<=2;i++) {   // Laço para verificar a posição da peça vazia
+					for(int i=0;i<=2;i++) {  
 						for(int j=0;j<=2;j++) {
 							tbParaBaixo[i][j] = f.tabuleiro[i][j];
 						}
@@ -332,16 +376,13 @@ public class Main {
 					
 					//JOptionPane.showMessageDialog(null, novaFronteiraParaBaixo.toString() +"\n"+ f.antecessor.toString());
 					
-					if(!novaFronteiraParaBaixo.toString().equals(f.antecessor.toString())) { //cASO SEJA IGUAL, ESSA FRONTEIRA JÁ EXISTE
-					
-
+					if(!novaFronteiraParaBaixo.toString().equals(f.antecessor.toString())) { 
+			
 						novaFronteiraParaBaixo.antecessor = f;
 						novaFronteiraParaBaixo.profundidade = f.profundidade + 1;
 						
 						//f.fronteiras.add(novaFronteiraParaBaixo);
-						
-						fronteiras.add(0,novaFronteiraParaBaixo);  //Adiciona a nova fronteira na lista de fronteiras
-						
+						fronteiras.add(0,novaFronteiraParaBaixo);  
 						//mostraMovimento(novaFronteiraParaBaixo);
 						
 					}	
@@ -350,11 +391,11 @@ public class Main {
 					
 				if((pJ + 1) <= 2) { //Verifica se existe uma posição à Direita
 					
-					Fronteira novaFronteiraDireita = new Fronteira(); //Cria nova fronteira
+					Fronteira novaFronteiraDireita = new Fronteira(); 
 					
 					String tbDireita [][] =  new String [3][3];
 					
-					for(int i=0;i<=2;i++) {   // Laço para verificar a posição da peça vazia
+					for(int i=0;i<=2;i++) {   
 						for(int j=0;j<=2;j++) {
 							tbDireita[i][j] = f.tabuleiro[i][j];
 							}
@@ -367,16 +408,13 @@ public class Main {
 					
 					//JOptionPane.showMessageDialog(null, novaFronteiraDireita.toString() +"\n"+ f.antecessor.toString());
 					
-					if(!novaFronteiraDireita.toString().equals(f.antecessor.toString())) { //cASO SEJA IGUAL, ESSA FRONTEIRA JÁ EXISTE
+					if(!novaFronteiraDireita.toString().equals(f.antecessor.toString())) { 
 					
-						
 						novaFronteiraDireita.antecessor = f;
 						novaFronteiraDireita.profundidade = f.profundidade + 1;
 						
 						//f.fronteiras.add(novaFronteiraDireita);
-						
-						fronteiras.add(0,novaFronteiraDireita);  //Adiciona a nova fronteira na lista de fronteiras
-						
+						fronteiras.add(0,novaFronteiraDireita);
 						//mostraMovimento(novaFronteiraDireita);
 					}
 						
@@ -388,7 +426,6 @@ public class Main {
 		}else {
 			fronteiras.remove(f);
 		}
-		
 	}
 	
 	public static void mostraMovimento(Fronteira fNew) {
@@ -407,13 +444,13 @@ public class Main {
 		JOptionPane.showMessageDialog(null, tabuleiroNew);
 	}
 	
-	public static boolean limiteDeProfundidade(ArrayList<Fronteira> fronteiras, int limite) {
+	public static boolean limiteDeProfundidade(ArrayList<Fronteira> fronteiras,Fronteira f, int limite) {
 		
 		boolean verifica = false;
 		
 		for(int i=0;i<fronteiras.size();i++) {
 			
-			if(fronteiras.get(i).profundidade < limite && fronteiras.get(i).expansao == false) {
+			if(fronteiras.get(i).profundidade < limite && fronteiras.get(i).expansao == false && f == null) {
 				verifica = true;
 			}
 		}
